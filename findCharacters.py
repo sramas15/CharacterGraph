babe3d52693b115ee6bbc6356b67007bd1cfbce8
Sheila Ramaswamy <ramasshe@stanford.edu>
2015-05-18 16:37:30 -0700

    Cleaned first few files

--- findCharacters.py
+++ findCharacters.py
@@ -1,13 +1,16 @@
 import re
 
-UPPERCASE_CNM = re.compile(r"  [A-Z]*\.")
-LOWERCASE_CNM = re.compile(r"  [A-Z][a-z]*\.")
+UPPERCASE_CNM = re.compile(r"  [A-Z ]*\.")
+UPPERCASE_CNM2 = re.compile(r"[A-Z ]*\.")
+LOWERCASE_CNM = re.compile(r"  ([A-Z][a-z ]*)+\.")
 
 def printCharSet(outF, characters):
 	for char in characters:
 		outF.write("%s\n" % char)
 
 def GetCharacters(num_files=35, inPtrn="out-%d.txt", outPtrn="char-%d.txt"):
+	match = UPPERCASE_CNM.search("                          SCENE XIII.")
+	print match.group()[2] != " "
 	for i in range(num_files):
 		inFNm = inPtrn % i
 		outFNm = outPtrn % i
@@ -19,16 +22,33 @@ def GetCharacters(num_files=35, inPtrn="out-%d.txt", outPtrn="char-%d.txt"):
 					title = line.strip()
 					continue
 				match = UPPERCASE_CNM.search(line)
-				if match and match.start() == 0:
+				if match and match.start() == 0 and match.group()[2] != " ":
 					char = match.group().strip(" .")
 					characters.add(char)
 				match = LOWERCASE_CNM.search(line)
-				if match and match.start() == 0:
+				if match and match.start() == 0 and match.group()[2] != " ":
 					char = match.group().strip(" .")
 					characters.add(char)
 		with open(outFNm, "w") as fOut:
 			fOut.write("%s\n" % title)
 			printCharSet(fOut, characters)
 
+def GetCharactersSingleFile(inFNm="out-3.txt", outFNm = "char-3.txt"):
+	title = None
+	characters = set()
+	with open(inFNm, "r") as fIn:
+		for line in fIn:
+			if not title:
+				title = line.strip()
+				continue
+			match = UPPERCASE_CNM2.search(line)
+			if match and match.start() == 0:
+				char = match.group().strip(" .")
+				characters.add(char)
+	with open(outFNm, "w") as fOut:
+		fOut.write("%s\n" % title)
+		printCharSet(fOut, characters)
+
+
 if __name__ == "__main__":
-	GetCharacters()
+	GetCharactersSingleFile()
