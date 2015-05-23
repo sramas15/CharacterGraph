95930a4b6730dbc33c3c0c962202f11a876b3024
northerncat <northerncat.brad820309@gmail.com>
2015-05-23 16:29:09 -0700

    LIWC Featurizer write
    Implemeted writing features to csv files.

--- liwcFeaturizer.py
+++ liwcFeaturizer.py
@@ -1,5 +1,6 @@
 import pickle
 import re
+import csv 
 
 # This function loads the LIWC dictionary and returns as a
 # dict from strings of scales to regex patterns
@@ -23,10 +24,14 @@ def liwcFeaturize(dictionary, text):
 		features[scale] = count / length
 	return features
 
+# This function parse the triple-*.txt files and convert the
+# files into a list of tuples containing pairs of speakers 
+# (following the convention speaker_listener) and the speech.
 def readTriples(filename):
 	speeches = []
 	with open(filename, 'r') as f:
 		while True:
+			# process each speech block of the triple-*.txt file
 			speaker = f.readline().strip()
 			if speaker == "":
 				break
@@ -46,14 +51,37 @@ def readTriples(filename):
 				speeches.append((pair, speech))
 	return speeches
 
-if __name__ == "__main__":
-	dictionary = getLIWCDictionary()
-	speeches = readTriples('triples-0-small.txt')
+# This fcuntion featurizes the speeches between characters
+# obtained from the triple-*.txt files using the LIWC features.
+def featurizeSpeeches(dictionary, speeches):
 	features = []
 	for pair in speeches:
 		speakers = pair[0]
 		speech = pair[1]
 		feature = liwcFeaturize(dictionary, speech)
-		features.append((pair, feature))
-	print len(features)
-	print features[2]
+		features.append((speakers, feature))
+	return features
+
+# This function writes the LIWC features of character speeches
+# into a CSV file that can be later parsed.
+def writeLIWCFeatures(outFilename, features, dictionary):
+	output = open(outFilename, 'wb')
+	csvWriter = csv.writer(output, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+	# write the headers
+	headers = ['speaker_listener']
+	for scale in dictionary:
+		headers.append(scale)
+	csvWriter.writerow(headers)
+	# write the features
+	for featurePair in features:
+		row = [featurePair[0]]
+		features = featurePair[1]
+		for scale in features:
+			row.append(features[scale])
+		csvWriter.writerow(row)
+
+if __name__ == "__main__":
+	dictionary = getLIWCDictionary()
+	speeches = readTriples('triples-0.txt')
+	features = featurizeSpeeches(dictionary, speeches)
+	writeLIWCFeatures('triple-0.csv', features, dictionary)
