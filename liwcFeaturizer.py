e86993c48fb896b4d0779fadb422cf566a748c9f
northerncat <northerncat.brad820309@gmail.com>
2015-05-25 02:05:28 -0700

    LIWC Features Len Normalized
    Created length normalized feature files for both individual and merged.

--- liwcFeaturizer.py
+++ liwcFeaturizer.py
@@ -12,7 +12,6 @@ def getLIWCDictionary():
 # of scales. Then normalize by the length of the text.
 def liwcFeaturize(dictionary, text):
 	words = re.split('[^A-Za-z0-9\']', text)
-	length = len(words)
 	features = {}
 	for scale in dictionary:
 		pattern = dictionary[scale].pattern
@@ -21,8 +20,8 @@ def liwcFeaturize(dictionary, text):
 			match = re.match(pattern, word)
 			if match:
 				count += 1.
-		features[scale] = count / length
-	return features
+		features[scale] = count
+	return features, len(words)
 
 # This function parse the triple-*.txt files and convert the
 # files into a list of tuples containing pairs of speakers 
@@ -58,29 +57,80 @@ def featurizeSpeeches(dictionary, speeches):
 	for pair in speeches:
 		speakers = pair[0]
 		speech = pair[1]
-		feature = liwcFeaturize(dictionary, speech)
-		features.append((speakers, feature))
+		feature, length = liwcFeaturize(dictionary, speech)
+		features.append((speakers, length, feature))
 	return features
 
+# This function merges two entries with the same speaker and listener
+# into one entry (in the form of triples)
+def makeCSVRow(characters, length, features):
+	row = [characters, length]
+	for scale in features:
+		row.append(features[scale])
+	return row
+
 # This function writes the LIWC features of character speeches
-# into a CSV file that can be later parsed.
-def writeLIWCFeatures(outFilename, features, dictionary):
-	output = open(outFilename, 'wb')
-	csvWriter = csv.writer(output, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+# into a CSV file that can be later parsed. It creates two files, one with
+# each speech act as an entry, while another with each speaker_listener pair
+# as an entry.
+def writeLIWCFeatures(indFilename, mergedFilename, features, dictionary):
+	outputInd = open(indFilename, 'wb')
+	indWriter = csv.writer(outputInd, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+	outputMerged = open(mergedFilename, 'wb')
+	mergedWriter = csv.writer(outputMerged, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
 	# write the headers
-	headers = ['speaker_listener']
+	headers = ['speaker_listener', 'Length']
 	for scale in dictionary:
 		headers.append(scale)
-	csvWriter.writerow(headers)
-	# write the features
+	indWriter.writerow(headers)
+	mergedWriter.writerow(headers)
+	# iterate through each speech act. write it to the individual csv file and merge it
+	# into the featureMap for the merged csv file
+	featureMap = {}
 	for featurePair in features:
-		row = [featurePair[0]]
-		features = featurePair[1]
-		for scale in features:
-			row.append(features[scale])
-		csvWriter.writerow(row)
+		characters = featurePair[0]
+		length = featurePair[1]
+		features = featurePair[2]
+		# merge the entry into featureMap, or simply add it to the featureMap
+		if characters in featureMap:
+			mergedLength = featureMap[characters][0] + length
+			mergedFeatures = {}
+			for scale in features:
+				mergedFeatures[scale] = features[scale] + featureMap[characters][1][scale]
+			featureMap[characters] = (mergedLength, mergedFeatures)
+		else:
+			featureMap[characters] = (length, features)
+		# write to the individual files
+		indWriter.writerow(makeCSVRow(characters, length, features))
+	# after processing each speech act, write the merged speech acts and features to the
+	# merged file
+	for characters in featureMap:
+		featurePair = featureMap[characters]
+		mergedWriter.writerow(makeCSVRow(characters, featurePair[0], featurePair[1]))
 
 if __name__ == "__main__":
 	dictionary = getLIWCDictionary()
 	for i in xrange(36):
+		speeches = readTriples('triples/triples-' + str(i) + '.txt')
 		features = featurizeSpeeches(dictionary, speeches)
+
+	# combine files for each play into an aggregate
+	indWriter = csv.writer(indComb, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+	mergedWriter = csv.writer(mergedComb, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+
+	# write the headers
+	headers = ['speaker_listener', 'Length']
+	for scale in dictionary:
+		headers.append(scale)
+	indWriter.writerow(headers)
+	mergedWriter.writerow(headers)
+
+	for i in xrange(36):
+			indFile.readline()
+			lines = indFile.readlines()
+			for line in lines:
+				indComb.write(line)
+			mergedFile.readline()
+			lines = mergedFile.readlines()
+			for line in lines:
+				mergedComb.write(line)
